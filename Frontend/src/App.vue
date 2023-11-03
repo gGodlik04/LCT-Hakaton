00251b742173685c8d9154c22b3e0af04a1629e0
Eduard_Galikhanov <Galikhanov@infomatika.ru>
2023-11-03 14:02:37 +0300

    feat|chore: Поднять проект, наладить работу api

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+
+const HelloWorld = async () => {
+  const res = await fetch('api/hello')
+  const jsonRes = await res.json();
+  await console.log(jsonRes)
+}
 </script>
 
 <template>
@@ -8,8 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <HelloWorld msg="You right it!" />
+        <div @click="HelloWorld" style="font-size: 50px; color: red; font-weight: 900;">ApiHelloWorld CLICK ON INSCRIPTION</div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
