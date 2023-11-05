18ea0a352003e25757fe3e2a9f363eda2120d035
Eduard Galikhanov <godlik04@mail.ru>
2023-11-04 19:09:21 -0700

    feat: Отчистить проект, начать работу

---
+++ Frontend/src/components/UI/index.js
@@ -0,0 +1,8 @@
+import ButtonUI from '@/components/UI/ButtonUI.vue';
+import ModalUI from '@/components/UI/ModalUI.vue';
+
+
+export default [
+    ButtonUI,
+    ModalUI,
+]
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,12 +1,17 @@
-import './assets/main.css'
+import './assets/main.sass'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import components from '@/components/UI'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+components.forEach(component => {
+    app.component(component.name, component)
+});
 
 app.use(createPinia())
 app.use(router)
