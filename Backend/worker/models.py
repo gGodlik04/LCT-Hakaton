4c850cc333cd9fbb7d326684aa6f210034dba060
MansorY23 <arsa20034@mail.ru>
2023-11-05 22:36:43 +0300

    create api

--- Backend/worker/models.py
+++ Backend/worker/models.py
@@ -3,22 +3,21 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 
 
-
 class Task(models.Model):
 
     uuid = models.UUIDField('UUID', primary_key=True, default=uuid.uuid4, editable=False)
-    agent_point_date = models.CharField('Дата подключения точки', max_length=50)
-    materials = models.BooleanField('Материалы доставлены')
-    last_card_given = models.SmallIntegerField('Дата последней выдачи карты')
-    num_given_cards = models.SmallIntegerField('Выданные карты')
-    approved_requests = models.SmallIntegerField('Одобренные заявки')
-    employee = models.BigIntegerField('Исполнитель задания')
-    priority = models.SmallIntegerField('Приоритет', )
+    agent_point_date = models.CharField('Дата подключения точки', max_length=50, default='Недавно', null=False)
+    materials = models.BooleanField('Материалы доставлены', null=False, default=True)
+    last_card_given = models.DateField('Дата последней выдачи карты', default='2000-01-01', null=False)
+    num_given_cards = models.SmallIntegerField('Выданные карты', null=False, default=8)
+    approved_requests = models.SmallIntegerField('Одобренные заявки', null=False, default=10)
+    employee = models.BigIntegerField('Исполнитель задания', null=True)
+    priority = models.SmallIntegerField('Приоритет', null=True)
     address = models.CharField('Адрес', max_length=255, default='Dagestanskaya')
-    work_time = models.SmallIntegerField('Время на выполнение')
-    appointment_date = models.DateField('Время начала задачи', null=True)
-    status = models.SmallIntegerField('Статус задачи')
-    task_type = models.CharField('Тип задачи', max_length=50)
+    work_time = models.SmallIntegerField('Время на выполнение', null=False, default=2)
+    appointment_date = models.DateField('Время начала задачи', null=True, default='2000-01-01')
+    status = models.SmallIntegerField('Статус задачи', default=1, null=True)
+    task_type = models.CharField('Тип задачи', max_length=50, default='Забрать карта', null=False)
 
     class Meta:
         permissions = [
--- Backend/worker/serializers.py
+++ Backend/worker/serializers.py
@@ -7,20 +7,10 @@ class CreateTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = [
-            'uuid', 'agent_point_date',
+            'agent_point_date', 'address',
             'materials', 'last_card_given', 'num_given_cards',
             'approved_requests', 'employee', 'priority'
         ]
-
-        def create(self, validated_data):
-            task = Task(
-                agent_point_date=validated_data['agent_point_date'],
-                materials=validated_data['materials'],
-                last_card_given=validated_data['last_card_given'],
-                num_given_cards=validated_data['num_given_cards'],
-                adress=validated_data['address']
-            )
-            task.save()
 
 
 class TaskSerializer(serializers.ModelSerializer):
