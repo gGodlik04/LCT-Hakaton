7e3c58c858ff6bb94dbdcfd9fb0f8e702e6ebe79
Eduard_Galikhanov <Galikhanov@infomatika.ru>
2023-11-07 17:33:59 +0300

    feat: merge from backend conflicts

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,9 +1,5 @@
-from django.contrib.auth import login
-from django.contrib.auth.models import AnonymousUser
-
 from rest_framework import viewsets, permissions, mixins
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 
 from accounts.serializers import UserSerializer
 from .models import User
