af33dcf2c1c4736a43ed8e347b83df4a7c37c19b
MansorY23 <arsa20034@mail.ru>
2023-11-11 21:11:05 +0300

    algoritm added and scheduled

--- Backend/tasks/tasks.py
+++ Backend/tasks/tasks.py
@@ -1,17 +1,41 @@
 from .algoritm import TaskDistributor
 from environs import Env
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def send_tasks():
     env = Env()
     env.read_env()
-    task_distributor = TaskDistributor('tasks/agent_points.csv',
-                                       'tasks/employers.csv',
-                                       yandex_api_key=env('API_YANDEX_KEY'),
-                                       openroute_api_key=env('API_OPENROUTE_KEY'))
-    task_distributor.run_distribution()
-    print(env.str('API_OPENROUTE_KEY'))
 
+    connection_string = create_engine(
+        'postgresql://fzpvuodf:ZJhLvoXnBVUJRGxCkDNpMvWmddimNytV@cornelius.db.elephantsql.com/fzpvuodf')
+
+    query_for_users = 'SELECT * FROM accounts_user'
+    query_for_points = 'SELECT * FROM "AgentPoint"'
+    query_for_task = 'SELECT * FROM "Task"'
+    query_for_task_type = 'SELECT * FROM "TaskType"'
+
+    df_users = pd.read_sql(query_for_users, connection_string)
+    df_users = df_users[df_users["role"] == 2]
+
+    df_points = pd.read_sql(query_for_points, connection_string)
+    df_task = pd.read_sql(query_for_task, connection_string)
+    df_task_type = pd.read_sql(query_for_task_type, connection_string)
+
+    df_points['uuid'] = df_points['uuid'].astype(str)
+    df_task['agent_point_id'] = df_task['agent_point_id'].astype(str)
+    points_task = pd.merge(df_points, df_task,
+                           left_on='uuid', right_on='agent_point_id', how='outer', suffixes=('_x', ''))
+
+    task_distributor = TaskDistributor(points_task, df_users, df_task, df_task_type,
+                                       'your_yandex_api',
+                                       'your_openroute_api')
+
+    task_distributor.run_distribution()
+    connection_string.dispose()
+
+    return
 
 
 
