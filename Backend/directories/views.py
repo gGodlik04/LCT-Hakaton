ede07eb478894323ba37617baac3f6a0b92cd6d8
MansorY23 <arsa20034@mail.ru>
2023-11-16 16:00:41 +0300

    docker compose settings

--- Backend/directories/views.py
+++ Backend/directories/views.py
@@ -1,4 +1,3 @@
-from rest_framework.response import Response
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import TaskTypeModel, AgentPointModel
